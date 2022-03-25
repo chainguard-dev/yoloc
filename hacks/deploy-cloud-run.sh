88ebebd70f213001a831c6c55f21ba2e9f8f837a
Thomas Stromberg <t+github@chainguard.dev>
2022-03-25 10:59:42 -0700

    Add deploy script

---
+++ hacks/deploy-cloud-run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export KO_DOCKER_REPO="gcr.io/yolo-checker/yoloc"
+# haha
+export GITHUB_TOKEN=ghp_nnrr3lmzqzy3rrr9rzunrp6rmzr99n3zs9zr
+gcloud run deploy yoloc --image="$(ko publish .)" --args=-serve \
+  --region us-central1 --project yolo-checker
